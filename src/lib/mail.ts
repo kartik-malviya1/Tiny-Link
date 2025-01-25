2c2c34cd5ac57d408b121b56d0a2ede97eb80108
kartik-malviya1 <kartikmalviya30@gmail.com>
2025-01-26 02:49:49 +0530

    resent domain email added

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,13 +1,14 @@
 import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
-const baseUrl = process.env.NEXTAUTH_URL || process.env.VERCEL_URL || "http://localhost:3000"
+const baseUrl =
+  process.env.NEXTAUTH_URL || process.env.VERCEL_URL || "http://localhost:3000"
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${baseUrl}/auth/new-password?token=${token}`
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@tiny-link.kartikmalviya.tech",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
