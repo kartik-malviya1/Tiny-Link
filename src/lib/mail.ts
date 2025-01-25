125bf0b906c0ec4009c2f157b41b77faac688ca7
kartik-malviya1 <kartikmalviya30@gmail.com>
2025-01-26 02:53:39 +0530

    email domain add

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -8,7 +8,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${baseUrl}/auth/new-password?token=${token}`
 
   await resend.emails.send({
-    from: "mail@tiny-link.kartikmalviya.tech",
+    from: "tiny-link.kartikmalviya.tech",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
